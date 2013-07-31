4a8386bab9f568688d71d5f87f6cc7dcce1d4d96
gk <gk@xyz.de>
2013-07-31 23:47:22 +0200

    works

--- nptest.pyx
+++ nptest.pyx
@@ -16,6 +16,31 @@ def nonlineq(np.ndarray[np.complex128_t, ndim=2] zmat, np.ndarray[np.complex128_
 					zmat[i,j].imag=nonlin_imag(zmat[i,j].real, zmat[i,j].imag, cmat[i,j].imag)
 					zmat[i,j].real=a
 					itermat[i,j]+=1
+					
+@cython.boundscheck(False)
+def nonlineq2(np.ndarray[np.complex128_t, ndim=2] zmat, np.ndarray[np.uint16_t, ndim=2] itermat, double boundleft, double boundright, double boundup, double bounddown, double limitsquare, int maxiter):
+	cdef unsigned int i,j
+#~ 	cdef Py_ssize_t i,j
+	cdef double a, creal, cimag
+	for iteration in range(maxiter):
+		for i in range(zmat.shape[0]):
+			for j in range(zmat.shape[1]):
+				creal=boundleft+(boundright-boundleft)/zmat.shape[0]*i
+				cimag=bounddown+(boundup-bounddown)/zmat.shape[1]*j
+				if square(zmat[i,j].real, zmat[i,j].imag) < limitsquare:
+#~ 					zmat[i,j]=nonlina(zmat[i,j],cmat[i,j])
+					a=nonlin_real(zmat[i,j].real, zmat[i,j].imag, creal)
+					zmat[i,j].imag=nonlin_imag(zmat[i,j].real, zmat[i,j].imag, cimag)
+					zmat[i,j].real=a
+					itermat[i,j]+=1
+
+#~ def nonlineq3(np.ndarray[np.uint16_t, ndim=2] itermat, double boundleft, double boundright, double boundup, double bounddown, double limitsquare, int maxiter):
+#~ 	cdef unsigned int i,j
+#~ 	cdef double a, creal, cimag
+#~ 	for i in range(itermat.shape[0]):
+#~ 		for j in range(itermat.shape[1]):
+#~ 			
+#~ 			for iteration in range(maxiter):
 
 cdef double square(double a, double b):
 	return a*a+b*b
